c5dfe67c7f18e3d9b5be8c9fbe66b834c2049e8b
superbryanlee <bryanlee.fh@gmail.com>
2022-05-04 17:47:23 -0700

    Added functionality.
    Textfields no longer reset when search results are loaded.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const colors = require('colors');
 const ejs = require('ejs');
 
 const apiKey = '6xRLOT2hzxA37NUpkb0rsyv92fqMhNv8O_c-2_PC04Ryt--xS5xQUQru2A8orO8EojnhSO5mjsUyFUvGFr0MSosv6b4FDMItXK60QMwRmqb7U4yMZ4M_Et_NOTU_YnYx';
-const clientId = 'k0nxX4jW8f0f45dVAD5_tQ';
 
 const handleHome = (req, res) => {
-    res.render('home.ejs', {showResult:false, noResult:false}); 
+    res.render('home.ejs', {showResult:false, noResult:false, 
+        locationQuery: null, termQuery: null, radiusQuery: null}); 
 }
 
 const handleSearch = async (req, res) => {
@@ -27,9 +27,38 @@ const handleSearch = async (req, res) => {
       };
 
     try {
-        await makeRequest(searchRequest, res);
+    const client = yelp.client(apiKey);
+    const response = await client.search(searchRequest);
+    const count = Object.keys(response.jsonBody.businesses).length;
+    const randomResult = Math.floor(Math.random() * count);
+    const result = await response.jsonBody.businesses[randomResult];
+
+    const restaurantURL = await genGoogleLink(result.coordinates.latitude, result.coordinates.longitude, result.name);
+    const name = (result.name);
+    const image = (result.image_url);
+    const rating = (result.rating);
+    const price = (result.price);
+    const location_array = result.location.display_address;
+    let address = "";
+    for (let i = 0; i < (location_array.length - 1); i++) {
+        address += location_array[i] + " ";
+    }
+    console.log(name);
+    console.log(address);
+    console.log(price);
+    console.log(restaurantURL);
+    console.log(rating);
+    const starRating = "⭐".repeat(parseInt(rating));
+    console.log(starRating);
+
+    res.render('home', {
+        showResult: true, noResult: false,
+        name, image, address, price, restaurantURL, starRating,
+        locationQuery: location, termQuery: terms, radiusQuery: radius});
+    console.log("Restaurant located!".cyan);
     } catch (e) {
-        res.render('home', {showResult: false, noResult: true});
+        res.render('home', {showResult: false, noResult: true,
+            locationQuery: location, termQuery: terms, radiusQuery: radius});
         console.log("No restaurants found with given parameters.".yellow);
     }
 }
@@ -57,7 +86,6 @@ app.post("/", handleSearch);
 
 // -----------------------------------------------------
 // FUNCTIONS:
-
 // genGoogleLink
 // @signature: int int => str
 // EFFECTS: Returns Google-Maps link to given place.
@@ -70,34 +98,6 @@ function genGoogleLink(x, y, place) {
 // makeRequest
 // @signature searchRequest-obj response-obj => void
 // EFFECTS: Makes an API request given a searchRequest object and renders response.
-async function makeRequest(searchRequest, res) {
-    const client = yelp.client(apiKey);
-    const response = await client.search(searchRequest);
-    const count = Object.keys(response.jsonBody.businesses).length;
-    const randomResult = Math.floor(Math.random() * count);
-    const result = await response.jsonBody.businesses[randomResult];
-
-    const restaurantURL = await genGoogleLink(result.coordinates.latitude, result.coordinates.longitude, result.name);
-    const name = (result.name);
-    const image = (result.image_url);
-    const rating = (result.rating);
-    const price = (result.price);
-    const location_array = result.location.display_address;
-    let address = "";
-    for (let i = 0; i < (location_array.length - 1); i++) {
-        address += location_array[i] + " ";
-    }
-    console.log(name);
-    console.log(address);
-    console.log(price);
-    console.log(restaurantURL);
-    console.log(rating);
-    const starRating = "⭐".repeat(parseInt(rating));
-    console.log(starRating);
+// async function makeRequest(searchRequest, res) {
     
-    res.render('home', {
-        showResult: true, noResult: false,
-        name, image, address, price, restaurantURL, starRating
-    });
-    console.log("Restaurant located!".cyan);
-}
+// }
